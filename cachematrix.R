cd951a0409d8b7defd463511c7a743f2604e4a29
monetina <fabianazollo@Fabianas-MacBook-Pro.local>
2015-03-22 18:42:21 +0100

    My assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allow to compute a matrix inverse while storing 
+## the matrix data and some ancillary functions in a list
 
-## Write a short comment describing this function
+## The function makeCacheMatrix provides a set of functions 
+## (set, setinverse, get, getinverse) and stores the matrix, 
+## giving as output a list
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+#sets the variable inv to NULL
+        inv <- NULL
+#defines the function set
+        set <- function(y) {
+#x (resp., inv) is super-assigned to be equal to y (resp., NULL)
+                x <<- y                                               
+                inv <<- NULL                            
+        }                                               
+        get <- function() x
+# the function setinverse() sets the value of inv
+        setinverse <- function(inverse) inv <<- inverse
+# the function getinverse() returns the value of inv
+        getinverse <- function() inv  
+#create a list that allows to retrieve functions and data
+        list(set = set, get = get,                      
+             setinverse = setinverse,                  
+             #
+             getinverse = getinverse)                   
 }
 
 
-## Write a short comment describing this function
+## The function takes as input the list produced by makeCacheMatrix, 
+## checks if the inverse matrix is stored in the cache and displays it
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#assign to the variable inv the value of the output of getinverse
+        inv <- x$getinverse()                   
+#If inv is not null, the variable inv is retrieved from cache
+        if(!is.null(inv)) {                     
+                message("getting cached data")  
+                return(inv)                     
+        }                                       
+        data <- x$get()                         
+#the matrix is inverted using solve()
+        inv <- solve(data, ...)
+#inv is superassigned with the value obtained from computation and then displayed
+        x$setinverse(inv)                      
+        inv                                     
 }
